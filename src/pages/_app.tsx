0ad903e7e007caee2054fe36909649edf352fa0e
acasune <tttt4aaaa@gmail.com>
2023-07-09 14:34:26 +0900

    add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,45 @@
 ﻿import { AppProps } from 'next/app'
 import '@/src/styles/style.css'
 import '@/src/styles/prism-monokai.css'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+import { useEffect } from 'react'
+import * as gtag from 'src/lib/gtag'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouterChange = (url: any) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouterChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouterChange)
+    }
+  }, [router.events])
+  return (
+    <>
+      {' '}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+      `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
